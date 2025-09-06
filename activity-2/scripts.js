4c986780dc818e4de7407341943b42f4204d08e6
tellstrs <tellstrs@matc.edu>
2025-09-06 15:56:44 -0500

    Start activity-2

---
+++ activity-2/scripts.js
@@ -0,0 +1,42 @@
+// Activity 2 : Operators and Control Flow
+
+let barrier = "|=====================================================================|";
+
+console.log(barrier);
+console.log("Part A : Arithmetic Operators");
+console.log(barrier);
+
+let a = 1;
+let b = 2;
+
+console.log("--- arithmetic operators examples ---")
+console.log("add 1+2: " + (a+b));
+console.log("subtract 1-2: " + (a-b));
+console.log("multiply 1*2: " + (a*2));
+console.log("divide 1/2: " + (1/2));
+console.log("modulas 1%2: " + (1%2));
+console.log("--- precedence examples : ")
+console.log("below will add 1 + 1 inside parenthesis before multiplying 2 then dividing 2");
+console.log("(1+1) * 2 / 2 = " + (a+a)*b/b);
+
+console.log(barrier);
+console.log("Part B : Comparison Operators");
+console.log(barrier);
+
+
+console.log(barrier);
+console.log("Part C : Logical Operators");
+console.log(barrier);
+
+console.log(barrier);
+console.log("Part D : Basic Conditional Statements");
+console.log(barrier);
+
+
+console.log(barrier);
+console.log("Part E : Switch Statement Demo");
+console.log(barrier);
+
+console.log(barrier);
+console.log("Part F : Age Checker Application");
+console.log(barrier);
